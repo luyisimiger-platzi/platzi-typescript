084d4432eb4f28262ef58040574db7855d5a39c7
Luis Mejia <luyisimiger@gmail.com>
2025-11-17 17:04:36 -0500

    docs: clase18

--- source/web/src/web.ts
+++ source/web/src/web.ts
@@ -1,1 +1,11 @@
-console.log("Hola desde mi explorador");
+// Seleccionando por nombre de etiqueta
+const h1 = document.querySelector('h1');
+console.log(h1?.textContent);
+
+// Seleccionando por clase
+let titulo: HTMLHeadingElement = document.querySelector('.title') as HTMLHeadingElement;
+console.log(titulo?.textContent);
+
+// Seleccionando por ID
+const message = document.querySelector('#message') as HTMLInputElement;
+console.log(message.placeholder);
